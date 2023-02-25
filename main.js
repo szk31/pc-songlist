ba79ac790c7bd02b2efa46d4dd4a04bb9acfc69b
shizuku3158 <ksgnshizuku@gmail.com>
2023-02-25 22:44:34 +0900

    script update 1.2.2
    attempt to fix different of dates (again)

--- main.js
+++ main.js
@@ -82,7 +82,7 @@ var video_idx = {
 	date : 1
 };
 
-var version = "1.2.1";
+var version = "1.2.2";
 
 /* control / memories */
 
@@ -532,8 +532,8 @@ function to8601(date_string) {
 // get day different between {date1 and date2} or {date1 and today}
 function get_date_different(date1, date2) {
 	date1 = (typeof(date1) === "string") ? new Date(date1) : date1;
-	date2 = date2 === undefined ? new Date() : new Date(date2);
-	return Math.round(Math.abs(date1.getTime() - date2.getTime()) / 86400000);
+	date2 = date2 === undefined ? new Date().setHours(0, 0, 0, 0) : new Date(date2);
+	return Math.floor(Math.abs(date1.getTime() - date2.getTime()) / 86400000);
 }
 
 // get entry count of all entry and member-only entry that fufills mask
